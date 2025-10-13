95cd213a76e902fe56596db17ed05e9202821176
ErosKollerDev <eroskoller.dev@gmail.com>
2025-10-13 16:31:28 -0300

    first commit

---
+++ library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -0,0 +1,24 @@
+package com.learnkafka.controller;
+
+
+import com.learnkafka.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/library-events")
+public class LibraryEventsController {
+
+    @PostMapping("/v1/post")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+        log.info("postLibraryEvent");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+}
